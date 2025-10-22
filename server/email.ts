8edd1f220a2df09accae82931ba5e622cb5e9f9b
canine <thomas@engineeringvirtue.net>
2025-10-22 16:49:18 -0400

    remove empty file lmao

--- server/email.ts
+++ server/email.ts
@@ -1,3 +1,6 @@
+// this used to be a really nice template
+// but it got blocked by mail servers
+// i think my emails still get captured by microsoft quarantine :(
 export const makeVerificationEmail = (link: string) =>
 	`<p>Thanks for your interest in HammerWars 2025! <a href="${link}" >Click here to verify your email.</a> If you encounter any issues, don't hesitate to contact us on <a href="https://discord.gg/A6twkCcW83" >Discord</a>.<br/> - <a href="https://purduecpu.com" >Purdue Competitive Programmers Union</a></p>
 
